810bde35ac6709b3bff3b3bf37c667c445151ddf
YoungHo_Jo <zeroho.jo@gmail.com>
2019-08-29 21:50:39 +0900

    Update MainController.java

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-  @GetMapping("")
+  @GetMapping("/")
   public ResponseEntity<String> hello() {
     return ResponseEntity.ok(
             "hello"
