5825eba3055488a313ce2226ca983d9d4f7a7120
YoungHo_Jo <zeroho.jo@gmail.com>
2019-08-29 21:44:33 +0900

    Update MainController.java

--- src/main/java/com/example/demo/controller/MainController.java
+++ src/main/java/com/example/demo/controller/MainController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/test")
 public class MainController {
 
   @GetMapping("")
